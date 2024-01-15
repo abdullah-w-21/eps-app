de5ad527363882e510868f2976f597a072a8e192
abdullah-w-21 <abdullahwasimm2112@gmail.com>
2024-01-15 09:11:47 -0800

    pushed

---
+++ src/lib/palm.ts
@@ -0,0 +1,69 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.PALM_API_KEY);
+
+interface OutputFormat {
+  [key: string]: string | string[] | OutputFormat;
+}
+
+export async function strict_output(
+  system_prompt: string,
+  user_prompt: string | string[],
+  output_format: OutputFormat,
+  default_category: string = "",
+  output_value_only: boolean = false,
+  model_name: string = "gemini-pro", // Adjust to your model name
+  temperature: number = 0.99,
+  num_tries: number = 3,
+  verbose: boolean = false
+): Promise<
+  {
+    question: string;
+    answer: string;
+  }[]
+> {
+  const list_input: boolean = Array.isArray(user_prompt);
+  const dynamic_elements: boolean = /<.*?>/.test(JSON.stringify(output_format));
+  const list_output: boolean = /\[.*?\]/.test(JSON.stringify(output_format));
+  let error_msg: string = "";
+
+  for (let i = 0; i < num_tries; i++) {
+    let output_format_prompt: string = `\nYou are to output the following in json format: ${JSON.stringify(
+      output_format
+    )}. \nDo not put quotation marks or escape character \\ in the output fields.`;
+
+    if (list_output) {
+      output_format_prompt += `\nIf output field is a list, classify output into the best element of the list.`;
+    }
+
+    if (dynamic_elements) {
+      output_format_prompt += `\nAny text enclosed by < and > indicates you must generate content to replace it. Example input: Go to <location>, Example output: Go to the garden\nAny output key containing < and > indicates you must generate the key name to replace it. Example input: {'<location>': 'description of location'}, Example output: {school: a place for education}`;
+    }
+
+    if (list_input) {
+      output_format_prompt += `\nGenerate a list of json, one json for each input element.`;
+    }
+
+    const model = genAI.getGenerativeModel({ model: model_name }); // Adjust to your model name
+
+    try {
+      const result = await model.generateContent([system_prompt, output_format_prompt, error_msg, user_prompt.toString()], {
+        temperature,
+      });
+
+      const response = await result.data;
+      const text = response.text;
+      console.log(text);
+
+     
+
+    } catch (e) {
+      error_msg = `\n\nError message: ${e}`;
+      console.log("An exception occurred:", e);
+    }
+  }
+
+  return [];
+}
